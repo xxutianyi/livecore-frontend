8bec549aab9c1344152a4d49f42fde54c3ffc5da
徐添翼 XuTianyi <xutianyi12@outlook.com>
2026-04-24 00:28:12 +0800

    Update form.tsx

--- src/app/(website)/profile/form.tsx
+++ src/app/(website)/profile/form.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { TextField } from '@/components/fields';
-import { Section } from '@/components/layout';
+import { Section, SectionTitle } from '@/components/layout';
 import { useUserContext } from '@/components/provider/user-provider';
 import { Button } from '@/components/shadcn/button';
 import { FieldGroup } from '@/components/shadcn/field';
@@ -11,51 +11,68 @@ import { cn } from '@/lib/utils';
 import { passwordUpdate, profileUpdate } from '@/service/api/auth';
 import { Lock, User } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { ComponentProps, ReactNode, useState } from 'react';
 import { toast } from 'sonner';
 
 export function ProfileForm() {
     const [section, setSection] = useState<'account' | 'password'>('account');
 
+    const AccountButton = (props: ComponentProps<typeof Button>) => (
+        <Button
+            variant="secondary"
+            className="md:hidden"
+            onClick={() => setSection('account')}
+            {...props}
+        >
+            <User /> 基本信息
+        </Button>
+    );
+    const PasswordButton = (props: ComponentProps<typeof Button>) => (
+        <Button
+            variant="secondary"
+            className="md:hidden"
+            onClick={() => setSection('password')}
+            {...props}
+        >
+            <Lock /> 修改密码
+        </Button>
+    );
+
     return (
-        <div className="mx-auto flex w-full max-w-5xl space-x-12">
-            <nav className="flex w-full max-w-1/5 flex-col space-y-1">
-                <Button
-                    size="lg"
-                    variant="ghost"
-                    className={cn(
-                        'justify-start',
-                        section === 'account'
-                            ? 'bg-muted hover:bg-accent'
-                            : 'hover:bg-accent hover:underline',
-                    )}
-                    onClick={() => setSection('account')}
-                >
-                    <User /> 基本信息
-                </Button>
-                <Button
-                    size="lg"
-                    variant="ghost"
-                    className={cn(
-                        'justify-start',
-                        section === 'password'
-                            ? 'bg-muted hover:bg-accent'
-                            : 'hover:bg-accent hover:underline',
-                    )}
-                    onClick={() => setSection('password')}
-                >
-                    <Lock /> 修改密码
-                </Button>
-            </nav>
-            <div className="w-full">
-                {section === 'account' && <UpdateProfile />}
-                {section === 'password' && <UpdatePassword />}
+        <>
+            <div className="mx-auto flex w-full space-x-12">
+                <nav className="hidden w-full max-w-1/5 flex-col space-y-1 md:flex">
+                    <AccountButton
+                        size="lg"
+                        variant="ghost"
+                        className={cn(
+                            'justify-start',
+                            section === 'account'
+                                ? 'bg-muted hover:bg-accent'
+                                : 'hover:bg-accent hover:underline',
+                        )}
+                    />
+                    <PasswordButton
+                        size="lg"
+                        variant="ghost"
+                        className={cn(
+                            'justify-start',
+                            section === 'password'
+                                ? 'bg-muted hover:bg-accent'
+                                : 'hover:bg-accent hover:underline',
+                        )}
+                    />
+                </nav>
+                <div className="w-full">
+                    {section === 'account' && <UpdateProfile action={<PasswordButton />} />}
+                    {section === 'password' && <UpdatePassword action={<AccountButton />} />}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
-export function UpdateProfile() {
+export function UpdateProfile({ action }: { action: ReactNode }) {
     const router = useRouter();
     const userContext = useUserContext();
 
@@ -74,7 +91,7 @@ export function UpdateProfile() {
     });
 
     return (
-        <Section title="基本信息">
+        <Section title={<SectionTitle title="基本信息" actions={[action]} />}>
             <Separator />
             <form className="w-full max-w-lg space-y-6" onSubmit={(e) => form.handleSubmit(e)}>
                 <FieldGroup>
@@ -91,7 +108,7 @@ export function UpdateProfile() {
     );
 }
 
-export function UpdatePassword() {
+export function UpdatePassword({ action }: { action: ReactNode }) {
     const form = useForm({
         onSubmit: async (values) => {
             try {
@@ -104,7 +121,7 @@ export function UpdatePassword() {
     });
 
     return (
-        <Section title="基本信息">
+        <Section title={<SectionTitle title="修改密码" actions={[action]} />}>
             <Separator />
             <form className="w-full max-w-lg space-y-6" onSubmit={form.handleSubmit}>
                 <FieldGroup>
